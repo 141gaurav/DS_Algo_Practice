eff42951b540996757ab618b94eff2c3fe98da75
Gaurav Chakraborty <19299987+141gaurav@users.noreply.github.com>
2021-08-16 15:27:33 +0000

    Wrote code to reverse a string

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-const strings = ['a', 'b', 'c', 'd'];
+//const strings = ['a', 'b', 'c', 'd'];
 
 //push
 //strings.push('g');
 //console.log(strings);
 
 //pop
-strings.pop();
-console.log(strings);
+//strings.pop();
+//console.log(strings);
 
 //add at first position
-strings.unshift('x');
-console.log(strings);
+//strings.unshift('x');
+//console.log(strings);
 
 //add string gaurav at index 2
 //strings.splice(2, 0, 'gaurav');
 //console.log(strings);
 
 //add string gaurav at index 2 and at over 1 element
-strings.splice(2, 1, 'gaurav');
-console.log(strings);
+//strings.splice(2, 1, 'gaurav');
+//console.log(strings);
+
+function reverseString(name){
+  return name.split('').reverse().join('');
+}
+
+console.log(reverseString('varuaG'))
